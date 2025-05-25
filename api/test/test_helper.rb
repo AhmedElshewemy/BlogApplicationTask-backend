e63bc9abcc65546a9b00ebbfa8c72ef9c78bad76
Ahmed Elshewemy <ahmedelshewemy11@gmail.com>
2025-05-25 18:49:58 +0300

    fix test

--- api/test/test_helper.rb
+++ api/test/test_helper.rb
@@ -1,17 +1,16 @@
+puts "[DEBUG] LOADED test_helper: #{__FILE__}"
+puts "[DEBUG] LOADED test_helper: #{__FILE__}"
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require 'json'
 
-module ActiveSupport
-  class TestCase
-    # Run tests in parallel with specified workers
+# module ActiveSupport
+#   class TestCase
+class ActiveSupport::TestCase
     parallelize(workers: :number_of_processors)
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    #fixtures :all
-
-    # Add more helper methods to be used by all tests here...
-
+   
    # self.use_transactional_fixtures = true
     self.use_transactional_tests = true
   # Parse JSON response body into a Hash
@@ -21,7 +20,7 @@ module ActiveSupport
 
     # Get a valid JWT token by signing up (or logging in) in tests
     def get_jwt_for_user(email:, password:)
-      # If the user doesn’t exist yet, create them
+
       user = User.find_by(email: email)
       unless user
         user = User.create!(name: "Test User", email: email, password: password, password_confirmation: password)
@@ -35,4 +34,3 @@ module ActiveSupport
     end
   end
   
-end
